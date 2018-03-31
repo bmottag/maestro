012da4b4cf7d6c99b5bc666b7310ef740d534692
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-03-31 09:29:02 -0600

    Ajuste relacion de tablas

--- bd/maestro.sql
+++ bd/maestro.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-03-2018 a las 17:31:25
+-- Tiempo de generación: 31-03-2018 a las 17:28:10
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `param_rol` (
   `id_rol` int(1) NOT NULL,
   `rol_name` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `first_name`, `last_name`, `log_user`, `email`, `fk_id_rol`, `birthdate`, `movil`, `password`, `state`, `photo`, `address`) VALUES
-(1, 'Benjamin', 'Motta', 'bmottag', 'benmotta@gmail.com', 1, '2018-03-19', '4033089921', '25f9e794323b453885f5181f1b624d0b', 1, '', '');
+(1, 'Benjamin', 'Motta', 'bmottag', 'benmotta@gmail.com', 1, '2018-03-19', '4033089921', '25d55ad283aa400af464c76d713c07ad', 1, '', '');
 
 --
 -- Índices para tablas volcadas
